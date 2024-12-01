9520cfe138e62d10fc12b125bf5e585b9a7e00e7
gar <hrothgarrrr@gmail.com>
2024-11-30 22:05:56 -0800

    shortened my code

--- 2024/1.py
+++ 2024/1.py
@@ -1,28 +1,18 @@
 file = "input1.txt"
 # file = "input1test.txt"
 
+with open(file, 'r') as f:
+    lines = f.read()
+
 
 ### PART 1
 
-with open(file, 'r') as f:
-    lines = f.readlines()
-
-aa = []
-bb = []
-for line in lines:
-    aa += [int(line.split(' ')[0])]
-    bb += [int(line.split(' ')[-1])]
-
-aa = sorted(aa)
-bb = sorted(bb)
+aa,bb = zip(*[tuple(l.split('   ')) for l in lines.split('\n')])
+aa,bb = sorted(int(a) for a in aa), sorted(int(b) for b in bb)
 total = sum(abs(aa[i]-bb[i]) for i in range(len(aa)))
 print(total)
 
 
 ### PART 2
 
-total = 0
-for a in aa:
-    total += a*sum(1 for b in bb if b == a)
-
-print(total)
+print( sum(a*sum(1 for b in bb if b == a) for a in aa) )
