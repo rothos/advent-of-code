f1044e0baedd02661318a9f2d9b6cb45819b7453
Rothos <hrothgarrrr@gmail.com>
2024-12-03 00:05:34 -0800

    minor update

--- 2024/3.py
+++ 2024/3.py
@@ -7,7 +7,7 @@ import re
 def process(text):
     pattern = re.compile(r"mul\(([0-9]+),([0-9]+)\)")
     matches = re.findall(pattern, text)
-    return sum([int(x[0])*int(x[1]) for x in matches])
+    return sum(int(x[0])*int(x[1]) for x in matches)
 
 ### PART 1
 print(process(text))
