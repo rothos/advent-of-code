18e2a6574d2a04f09140831bb8966f06e31b5745
gar <hrothgarrrr@gmail.com>
2024-12-07 13:24:28 -0800

    improving code for day 7 (learning from better code i saw)

--- 2024/7.py
+++ 2024/7.py
@@ -1,35 +1,29 @@
 lines = open("input7.txt", 'r').read().splitlines()
 # lines = open("input7test.txt", 'r').read().splitlines()
 
+import re
 
 def combine(s, nn, concat=False):
     if len(nn) == 1:
-        if nn[0] == s:
-            return True
-        return False
+        return nn[0] == s
     
     if nn[0] > s:
         return False
 
-    x = combine(s, [nn[0]*nn[1]]+nn[2:], concat=concat)
-    y = combine(s, [nn[0]+nn[1]]+nn[2:], concat=concat)
-    z = False
-    if concat:
-        z = combine(s, [int(str(nn[0])+str(nn[1]))]+nn[2:], concat=concat)
-
-    return x or y or z
+    return (
+        combine(s, [nn[0]*nn[1]]+nn[2:], concat=concat)
+        or combine(s, [nn[0]+nn[1]]+nn[2:], concat=concat)
+        or (concat and combine(s, [int(str(nn[0])+str(nn[1]))]+nn[2:], concat=concat))
+    )
 
 
-total = 0
-total2 = 0
+total = total2 = 0
 for line in lines:
-    test, numbers = line.split(':')
-    test = int(test)
-    numbers = [int(n) for n in numbers.split()]
-    if combine(test, numbers):
-        total += test
-    if combine(test, numbers, concat=True):
-        total2 += test
+    desired, *values = map(int, re.split(':? ', line))
+    if combine(desired, values):
+        total += desired
+    if combine(desired, values, concat=True):
+        total2 += desired
 
 print(total)
 print(total2)
