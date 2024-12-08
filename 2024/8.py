45ae1161e83869d93747d2df6ab44d53358abb72
gar <hrothgarrrr@gmail.com>
2024-12-07 21:52:27 -0800

    cleaning up code

--- 2024/8.py
+++ 2024/8.py
@@ -9,6 +9,17 @@ def in_grid(coord, grid):
     w,h = grid.shape
     return 0 <= coord[0] < w and 0 <= coord[1] < h
 
+def get_antinodes(a, b, grid, part2):
+    ants = set()
+    k = 1
+    while in_grid( k*(b-a)+a, grid ) and (part2 or k < 3):
+        if part2 or k == 2:
+            ants.add(tuple(k*(b-a)+a))
+        k+=1
+
+    return ants
+
+
 types = set(text).difference({'\n', '.'})
 grid = np.array([list(t) for t in text.splitlines()])
 
@@ -17,23 +28,8 @@ def day8(part2=False):
     for type in types:
         ants = np.argwhere(grid == type)
         for a,b in itertools.combinations(ants, 2):
-            if part2:
-                k = 1
-                while in_grid( k*(b-a)+a, grid ):
-                    antinodes.add(tuple(k*(b-a)+a))
-                    k+=1
-                k = 1
-                while in_grid( k*(a-b)+b, grid ):
-                    antinodes.add(tuple(k*(a-b)+b))
-                    k+=1
-
-            else:
-                an1 = 2*b - a
-                an2 = 2*a - b
-                if in_grid(an1, grid):
-                    antinodes.add(tuple(an1))
-                if in_grid(an2, grid):
-                    antinodes.add(tuple(an2))
+            antinodes = antinodes.union(get_antinodes(a, b, grid, part2))
+            antinodes = antinodes.union(get_antinodes(b, a, grid, part2))
 
     return len(antinodes)
 
