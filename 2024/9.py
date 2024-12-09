bfb4814ffa662faa7388f47c6a3bc0cd647d7c8e
gar <hrothgarrrr@gmail.com>
2024-12-08 22:33:09 -0800

    day 9 works again

--- 2024/9.py
+++ 2024/9.py
@@ -1,18 +1,8 @@
 text = open("input9.txt", 'r').read()
 # text = open("input9test.txt", 'r').read()
 
-import copy
 
-def make_disk(diskmap):
-    disk = []
-    i = 0
-    for n in diskmap:
-        if i % 2:
-            disk += int(n) * [None]
-        else:
-            disk += int(n) * [i//2]
-        i += 1
-    return disk
+### PART 1
 
 def do_one_swap(disk):
     e = disk.index(None)
@@ -23,6 +13,29 @@ def do_one_swap(disk):
         disk[k], disk[e] = disk[e], disk[k]
     return disk
 
+def get_sum1(disk):
+    disk = [d for d in disk if d != None]
+    return sum([i*n for i,n in enumerate(disk)])
+
+def do_part1(diskmap):
+    disk = []
+    i = 0
+    for n in diskmap:
+        if i % 2:
+            disk += int(n) * [None]
+        else:
+            disk += int(n) * [i//2]
+        i += 1
+
+    while disk.copy() != do_one_swap(disk):
+        disk = do_one_swap(disk)
+
+    return get_sum1(disk)
+
+
+### PART 2
+
+# Drawing function for debugging
 def draw_disk(disk):
     locs = sorted(disk.keys())
     s = ""
@@ -31,14 +44,28 @@ def draw_disk(disk):
         s += c * disk[loc]['len']
     return s
 
-def move_files_and_get_sum(diskmap):
-    diskmap = map(int, diskmap)
+def make_disk(diskmap):
     disk = dict()
     location = 0
     for i,n in enumerate(diskmap):
         disk[location] = {'id': i//2 if i%2==0 else None, 'len': n}
         location += n
+    return disk
 
+def get_sum2(disk):
+    total = 0
+    for loc in disk.keys():
+        file = disk[loc]
+        if file['id'] == None:
+            continue
+        k = 0
+        while k < file['len']:
+            total += (loc+k)*file['id']
+            k += 1
+    return total
+
+def do_part2(diskmap):
+    disk = make_disk(diskmap)
     locs = sorted(disk.keys())
     filelocs = [l for l in locs if disk[l]['id']]
     for fileloc in filelocs[::-1]:
@@ -56,34 +83,10 @@ def move_files_and_get_sum(diskmap):
 
                 break
 
-    # do the sum
-    total = 0
-    for loc in disk.keys():
-        file = disk[loc]
-        if file['id'] == None:
-            continue
-        k = 0
-        while k < file['len']:
-            total += (loc+k)*file['id']
-            k += 1
-
-    return total
-
-def get_sum(disk):
-    disk = copy.deepcopy([d for d in disk if d != None])
-    return sum([i*n for i,n in enumerate(disk)])
-
-def day9(text, part2=False):
-    diskmap = list(text.strip())
-    if not part2:
-        disk = make_disk(diskmap)
-        while disk.copy() != do_one_swap(disk):
-            disk = do_one_swap(disk)
-            return get_sum(disk)
-    else:
-        return move_files_and_get_sum(diskmap)
+    return get_sum2(disk)
 
 
 
-print(day9(text))
-# print(day9(text, part2=True))
+diskmap = list(map(int, list(text.strip())))
+print(do_part1(diskmap))
+print(do_part2(diskmap))
