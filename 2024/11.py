59166372409a0c29699e872eb7640dcf9182375f
gar <hrothgarrrr@gmail.com>
2024-12-10 21:39:47 -0800

    cleaning up 11 a little more

--- 2024/11.py
+++ 2024/11.py
@@ -5,21 +5,23 @@ from functools import cache
 import math
 
 @cache
-def do_iters(n, iters):
+def iterate(n, iters):
     if iters == 0:
         return 1
     if n == 0:
-        return do_iters(1, iters-1)
-    if math.floor(math.log(n,10)) % 2:
-        b = math.floor(math.log(n,10))
-        return sum([do_iters(n // 10**(b//2+1), iters-1),
-                    do_iters(n % 10**(b//2+1), iters-1)])
+        return iterate(1, iters-1)
+    if math.floor(math.log(n, 10)) % 2:
+        b = math.floor(math.log(n, 10))
+        return (
+                iterate(n // 10**(b//2+1), iters-1)
+              + iterate(n % 10**(b//2+1), iters-1)
+            )
     else:
-        return do_iters(n*2024, iters-1)
+        return iterate(n*2024, iters-1)
 
 import time
 start = time.perf_counter()
 nn = list(map(int, text.split()))
-print(sum(do_iters(n, 25) for n in nn))
-print(sum(do_iters(n, 75) for n in nn))
+print(sum(iterate(n, 25) for n in nn))
+print(sum(iterate(n, 75) for n in nn))
 print(f"Execution time: {time.perf_counter() - start:.4f} seconds")
