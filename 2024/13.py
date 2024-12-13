88e94e41adadc2bbbe9e45dda4a5cd7c8cc36f8e
gar <hrothgarrrr@gmail.com>
2024-12-12 22:04:44 -0800

    gr

--- 2024/13.py
+++ 2024/13.py
@@ -9,20 +9,20 @@ def parse_machine(machine_text):
     ax, ay = re.findall(r'\d+', line1)
     bx, by = re.findall(r'\d+', line2)
     prize_xy = re.findall(r'\d+', line3)
-    matrix = np.array([[ax, bx], [ay, by]], dtype=int)
+    matrix = np.array([[ax, bx], [ay, by]], dtype=float)
 
-    return matrix, np.array(prize_xy, dtype=int)
+    return matrix, np.array(prize_xy, dtype=float)
 
 def solve_machine(machine, const=0):
-    eps = 10e-6
+    eps = 10e-4
     matrix, prize_xy = machine
     prize_xy += const
     a,b = np.linalg.solve(matrix, prize_xy)
     if abs(a-round(a)) > eps or abs(b-round(b)) > eps:
         return 0
-    return round(3*a + b)
+    return 3*a + b
 
-const = 10000000000000
+const = 10_000_000_000_000
 total1 = 0
 total2 = 0
 for machine_text in text.split("\n\n"):
@@ -32,40 +32,3 @@ for machine_text in text.split("\n\n"):
 
 print(int(total1))
 print(int(total2))
-# 20669, wrong
-# 29505, wrong
-# 29512, wrong
-# 29517, wrong
-
-
-
-
-# from scipy.optimize import linprog
-
-# def solve_machine(machine):
-#     eps = 10e-6
-#     matrix, prize_xy = machine
-
-#     # Linear inequality constraints
-#     A_ub = None  # No additional inequality constraints
-#     b_ub = None  # No additional inequality bounds
-    
-#     # Objective function coefficients (to minimize 3x1 + x2)
-#     c = np.array([3, 1])
-    
-#     result = linprog(
-#         c,              # Objective function coefficients
-#         A_ub=A_ub,      # Inequality constraint matrix
-#         b_ub=b_ub,      # Inequality constraint vector
-#         A_eq=matrix,      # Equality constraint matrix
-#         b_eq=prize_xy,      # Equality constraint vector
-#         method='highs'  # Most reliable method for equality constraints
-#     )
-    
-#     if type(result.x) == type(None):
-#         return 0
-#     a,b = result.x
-#     if abs(a-round(a)) > eps or abs(b-round(b)) > eps:
-#         return 0
-#     return int(3*a + b)
-
