9a7abeefca863d81b140ba3595455b64409b8994
gar <hrothgarrrr@gmail.com>
2024-12-13 22:32:32 -0800

    finished 14 part 2..

--- 2024/14.py
+++ 2024/14.py
@@ -32,18 +32,38 @@ print(prod(quads))
 
 ### PART 2
 
+from scipy.stats import entropy
+
+def calculate_entropy(array, window_size):
+    h, w = array.shape
+    entropies = []
+    for i in range(0, h - window_size + 1, window_size):
+        for j in range(0, w - window_size + 1, window_size):
+            sub_window = array[i:i+window_size, j:j+window_size]
+            p_true = np.sum(sub_window) / sub_window.size
+            p_false = 1 - p_true
+            probabilities = np.array([p_true, p_false])
+            entropies.append(entropy(probabilities, base=2))
+    return np.mean(entropies)
+
+def print_grid(grid):
+    for row in grid:
+        print(''.join('X' if val else '.' for val in row))
+
 t = 0
-# no tree up to 3663
 while True:
-    robots = set()
+    if t % 100 == 0:
+        print(f't = {t}...')
+    grid = np.full([w,h], False)
     for line in lines:
         pos, vel = [np.array(re.findall(r'-?\d+', part), dtype=int) for part in line.split()]
         x,y = ( (pos + vel*t) % [w,h] )
-        robots.add((x,y))
-    for i in range(h):
-        line = ""
-        for j in range(w):
-            line += 'X' if (j,i) in robots else '.'
-        print(line)
-    _ = input(f'^ t = {t}')
+        grid[x,y] = True
+
+    e = calculate_entropy(grid, 5)
+
+    if e < 0.2:
+        print_grid(grid)
+        # _ = input(f'^ t = {t}')
+
     t += 1
