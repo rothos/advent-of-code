f6709a12dd38eddd76a17791b02bec67b3e171b4
gar <hrothgarrrr@gmail.com>
2024-12-16 00:24:45 -0800

    updating 16

--- 2024/16.py
+++ 2024/16.py
@@ -5,42 +5,34 @@ text = open("input16.txt", 'r').read()
 import numpy as np
 import heapq
 
-def shortest_path(start, is_end, neighbors_function):
+def shortest_path(start, is_end_fn, get_neighbors_fn):
     seen = set()
     queue = [(0, (start, [start]))]
     while queue:
         cost, (node, path) = heapq.heappop(queue)
         if node not in seen:
             seen.add(node)
-            if is_end(node):
+            if is_end_fn(node):
                 return path,cost
-            for neighbor,thiscost in neighbors_function(node):
-                heapq.heappush(queue, (cost + thiscost, (neighbor, path + [neighbor])))
+            for neighbor,move_cost in get_neighbors_fn(node):
+                heapq.heappush(queue, (cost + move_cost, (neighbor, path + [neighbor])))
 
-def all_shortest_paths(start, is_end, neighbors_function):
+def all_shortest_paths(start, is_end_fn, get_neighbors_fn):
     seen = {}
     queue = [(0, (start, [start]))]
     best_cost = float('inf')
     shortest_paths = []
-
     while queue:
         cost, (node, path) = heapq.heappop(queue)
         if cost > best_cost:
             break
-
         if (node not in seen or cost <= seen[node]):
             seen[node] = cost
-            
-            if is_end(node):
-                if not shortest_paths or cost <= best_cost:
-                    if cost < best_cost:
-                        shortest_paths = []
-                        best_cost = cost
-
-                    shortest_paths.append(path)
-            
-            for neighbor, thiscost in neighbors_function(node):
-                new_cost = cost + thiscost
+            if is_end_fn(node):
+                best_cost = cost
+                shortest_paths.append(path)
+            for neighbor,move_cost in get_neighbors_fn(node):
+                new_cost = cost + move_cost
                 if new_cost <= best_cost:
                     heapq.heappush(queue, (new_cost, (neighbor, path + [neighbor])))
     
@@ -51,6 +43,9 @@ def is_end(end_xy):
 
 def add(a,b):
     return (a[0]+b[0], a[1]+b[1])
+
+def negate(v):
+    return (-v[0], -v[1])
 
 def rot90(direction):
     if direction == (0,1):  return (1,0)
@@ -64,8 +59,7 @@ def get_neighbors(grid):
         # Can either turn or go forward
         xy,direction = node
         neighbors.append( ((xy, rot90(direction)), 1000) )
-        neighbors.append( ((xy, rot90(rot90(direction))), 1000) )
-        neighbors.append( ((xy, rot90(rot90(rot90(direction)))), 1000) )
+        neighbors.append( ((xy, negate(rot90(direction))), 1000) )
 
         if grid[xy] != "#":
             neighbors.append( ((add(xy, direction), direction), 1) )
