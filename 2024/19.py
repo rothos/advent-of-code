fbfe0be698db7f4aa5f1bcbc3a905477496d3771
gar <hrothgarrrr@gmail.com>
2024-12-18 21:21:49 -0800

    finished day 19

---
+++ 2024/19.py
@@ -0,0 +1,50 @@
+text = open("input19.txt", 'r').read()
+# text = open("input19test.txt", 'r').read()
+
+from functools import cache
+
+@cache
+def same_up_to_len(a,b):
+    l = min(len(a), len(b))
+    return a[:l] == b[:l]
+
+@cache
+def can_make(desired, available, cur=""):
+    if cur == desired:
+        return 1
+
+    elif len(cur) > len(desired):
+        return 0
+
+    total = 0
+    for a in available:
+        if same_up_to_len(cur + a, desired):
+            total += can_make(desired, available, cur + a)
+
+    return total
+
+def do_part(part):
+
+    available,desired = text.split("\n\n")
+    available = tuple(available.split(", "))
+    desired = tuple(desired.split("\n"))
+
+    results = [can_make(d,available) for d in desired]
+
+    if part == 1:
+        
+        return sum(1 for x in results if x)
+
+    else:
+        
+        return sum(results)
+
+
+
+import time
+start = time.perf_counter()
+print(do_part(1))
+print(f"Execution time: {time.perf_counter() - start:.4f} seconds")
+start = time.perf_counter()
+print(do_part(2))
+print(f"Execution time: {time.perf_counter() - start:.4f} seconds")
