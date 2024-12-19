04ecefd194b0c6781b60c508214a121d2c3f3f82
gar <hrothgarrrr@gmail.com>
2024-12-18 21:59:44 -0800

    improvements to 19

--- 2024/19.py
+++ 2024/19.py
@@ -4,29 +4,22 @@ text = open("input19.txt", 'r').read()
 from functools import cache
 
 @cache
-def same_up_to_len(a,b):
-    l = min(len(a), len(b))
-    return a[:l] == b[:l]
-
-@cache
 def can_make(desired, available, cur=""):
     if cur == desired:
         return 1
-    elif len(cur) > len(desired):
-        return 0
 
     total = 0
     for a in available:
-        if same_up_to_len(cur + a, desired):
+        if desired.startswith(cur + a):
             total += can_make(desired, available, cur + a)
 
     return total
 
 def do_part(part):
-    available,desired = text.split("\n\n")
+    available, desired = text.split("\n\n")
     available = tuple(available.split(", "))
     desired = tuple(desired.split("\n"))
-    results = [can_make(d,available) for d in desired]
+    results = [can_make(d, available) for d in desired]
 
     if part == 1:
         return sum(1 for x in results if x)
